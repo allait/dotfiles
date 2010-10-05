aad3993f5c602e93f8805beb61432d9e3d7fcc94
Alexey Bezhan <al@fueledbylemons.com>
2010-10-05 18:54:53 +0400

    Changed prompt color to yellow, alias comments

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,4 @@
+# Common aliases
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
 alias ack='ack-grep'
--- .zsh/appearance.zsh
+++ .zsh/appearance.zsh
@@ -36,7 +36,7 @@ function precmd {
   title "$USERNAME:$PWD"
 }
 #PROMPT='%{$fg[cyan]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info) %% '
-PROMPT='%{$fg_bold[cyan]%}%c%{$reset_color%}$(git_prompt_info) %{$fg_bold[red]%}%%%{$reset_color%} '
+PROMPT='%{$fg_bold[cyan]%}%c%{$reset_color%}$(git_prompt_info) %{$fg_bold[yellow]%}%%%{$reset_color%} '
 ZSH_THEME_GIT_PROMPT_PREFIX="@%{$fg[yellow]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[red]%}✗%{$reset_color%}"
