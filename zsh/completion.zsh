33cdefa36c14561a74ac4b33feeee1ee2f98b02f
Alexey Bezhan <al@fueledbylemons.com>
2011-01-01 22:45:32 +0300

    Added pip completio

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -13,3 +13,16 @@ zstyle ':completion:*' completer _oldlist _complete _match _ignored \
     _list _history
 autoload -U compinit
 compinit
+
+# pip zsh completion start
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip_completion pip
+# pip zsh completion end
+
