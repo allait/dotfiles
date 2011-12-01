6e627bca028bddbd637eb2cccf521a6c691b6b08
Alexey Bezhan <al@fueledbylemons.com>
2011-12-01 15:12:11 +0400

    Use status --porcelain for git prompt info

--- zsh/plugins/vcs_prompt_info.zsh
+++ zsh/plugins/vcs_prompt_info.zsh
@@ -9,7 +9,7 @@ vcs_prompt_info() {
 }
 
 git_parse_status() {
-    vcs_status=$(git status -s 2> /dev/null)
+    vcs_status=$(git status --porcelain 2> /dev/null)
     if [[ -z $vcs_status ]]; then
         echo "$VCS_PROMPT_CLEAN"
         return
