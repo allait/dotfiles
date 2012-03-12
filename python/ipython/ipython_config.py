df5c911e90ec4a9582851127f04711cacb01082f
Alexey Bezhan <al@fueledbylemons.com>
2012-03-12 15:30:38 +0400

    Remove virtualenv support in iPython, use local installations instead

--- python/ipython/ipython_config.py
+++ python/ipython/ipython_config.py
@@ -2,16 +2,19 @@
 
 c = get_config()
 
-# List of files to run at IPython startup.
-c.InteractiveShellApp.exec_files = [
-    "virtualenv.py",
-]
 
-# List of commands to execute at IPython startup.
+# Import modules at IPython startup.
 c.InteractiveShellApp.exec_lines = [
     'import os',
-    'import sys',
+    'import sys'
 ]
 
 # Whether to display a banner upon starting IPython.
-c.InteractiveShellApp.display_banner = False
+c.TerminalIPythonApp.display_banner = False
+
+# Set to confirm when you try to exit IPython with an EOF
+c.TerminalInteractiveShell.confirm_exit = False
+
+# Enable completion on elements of lists, results of function calls,
+# etc., but can be unsafe because the code is actually evaluated on TAB.
+c.IPCompleter.greedy = False
