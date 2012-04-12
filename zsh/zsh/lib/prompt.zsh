3e4b11a6198e1ff8338f205ad77cef7807429fcc
Alexey Bezhan <al@fueledbylemons.com>
2012-04-12 16:36:16 +0400

    ZSH prompt: change colors, replace git flags with jobs flag
    Git status is slow on big repositories.

--- zsh/zsh/lib/prompt.zsh
+++ zsh/zsh/lib/prompt.zsh
@@ -1,3 +1,28 @@
+# Parameter expantion and substitution performed in prompts
+setopt prompt_subst
+
+# Load vcs_info
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable hg git
+zstyle ':vcs_info:*' formats ' on %F{yellow}%b%f'
+
+# Execute function before every command
+function precmd () {
+    vcs_info
+}
+
+# Vcs flags to use in vcs_prompt_info function
+VCS_PROMPT_UNTRACKED="%F{blue}● %f"
+VCS_PROMPT_UNSTAGED="%B%F{red}● %b%f"
+VCS_PROMPT_UNCOMMITED="%F{red}● %f"
+VCS_PROMPT_CLEAN="%F{green}● %f"
+
+# Left prompt
+PROMPT='%F{25}$(prompt_pwd)%f${vcs_info_msg_0_} %F{yellow}%#%f '
+
+# Right prompt
+RPROMPT='%1(j,%F{blue}● %f,)%(?,,%F{red}[%?]%f)[%T]'
+
 # Vi mode indication
 function zle-line-init zle-keymap-select {
     RPS1="${${KEYMAP/vicmd/$VI_NORMAL_MODE}/(main|viins)/$VI_INSERT_MODE}"
@@ -5,32 +30,9 @@ function zle-line-init zle-keymap-select {
     zle reset-prompt
 }
 
-# Parameter expantion and substitution performed in prompts
-setopt prompt_subst
-
-# Load vcs_info
-autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable hg git
-zstyle ':vcs_info:*' formats '@%F{yellow}%b%f'
-
-# Execute function before every command
-function precmd () {
-    vcs_info
-    vcs_prompt_info
-}
-
-VCS_PROMPT_UNTRACKED="%F{blue}● %f"
-VCS_PROMPT_UNSTAGED="%B%F{red}● %b%f"
-VCS_PROMPT_UNCOMMITED="%F{red}● %f"
-VCS_PROMPT_CLEAN="%F{green}● %f"
-
-RPROMPT='${vcs_prompt_info_status_}[%T]'
-PROMPT='%F{blue}$(prompt_pwd)%f${vcs_info_msg_0_} %F{yellow}%%%f '
-
 # Vi mode indicators
 VI_NORMAL_MODE='%F{red}⚙%f'
 VI_INSERT_MODE=$RPROMPT
 
-autoload zle-line-init zle-keymap-select
 zle -N zle-line-init
 zle -N zle-keymap-select
