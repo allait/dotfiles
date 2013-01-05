9acd40c85fcc55d7b1714a8fb7e3d4772012f13b
Alexey Bezhan <al@fueledbylemons.com>
2013-01-06 02:14:51 +0400

    Rewrite emacs config files: automatically install packages from elpa

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,27 +1,5 @@
 ;; Adding ~/.emacs.d to load path
 (add-to-list 'load-path "~/.emacs.d/")
-(add-to-list 'load-path "~/.emacs.d/packages/")
-
-;; Load ELPA
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
-
-;; Load paredit
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
-(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
-(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
-(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
-
-;; Load color-theme
-(require 'color-theme)
-(color-theme-initialize)
-
-;; Set color theme
-(color-theme-gruber-darker)
 
 ;; Set font size
 (set-face-attribute 'default nil :height 110)
@@ -38,6 +16,10 @@
 ;; No startup message
 (setq inhibit-startup-message t)
 
+;; Save backup files to ~/tmp
+(setq backup-directory-alist `(("." . "~/tmp/backup")))
+(setq auto-save-file-name-transforms `((".*" "~/tmp/swap")))
+
 ;; Activate Org-mode
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -45,13 +27,10 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
-;; Associate markdown-mode with md files
-(autoload 'markdown-mode "markdown-mode.el"
- "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
+;; take the short answer, y/n is yes/no
+(defalias 'yes-or-no-p 'y-or-n-p)
 
-;; Load evil (vim mode)
-(add-to-list 'load-path "~/.emacs.d/packages/evil/")
-(require 'evil)
-(evil-mode 1)
+;; Load config parts
+(load "~/.emacs.d/packages.el")
+(load "~/.emacs.d/keys.el")
+
---
+++ emacs/emacs.d/packages.el
@@ -0,0 +1,80 @@
+(require 'package)
+
+
+;; Package installation
+;; --------------------
+
+;; Set the package sources.
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("marmalade" . "http://marmalade-repo.org/packages/")
+        ("elpa" . "http://tromey.com/elpa/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+
+;; Setup packages
+(setq elpa-packages
+      '(clojure-mode
+        deft
+        evil
+        markdown-mode
+        paredit
+        ))
+
+;; Initialize the package system.
+(package-initialize)
+
+;; Refresh package archives when necessary.
+(unless (file-exists-p "~/.emacs.d/elpa/archives")
+  (package-refresh-contents))
+
+;; Install all packages.
+(dolist (package elpa-packages)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+
+;; Package configuration
+;; ---------------------
+
+;; Load paredit
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
+
+;; Add themes/ to deftheme list
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+;; Set color theme
+(load-theme 'gruber-darker t)
+
+;; Associate markdown-mode with md files
+(autoload 'markdown-mode "markdown-mode.el"
+ "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+      (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+;; Setup deft
+(require 'deft)
+(setq deft-extension "md")
+(setq deft-text-mode 'markdown-mode)
+(setq deft-directory "~/wiki/scratch/")
+(setq deft-use-filename-as-title t)
+
+(global-set-key [f1] 'deft)
+
+;; Load evil (vim mode)
+(add-to-list 'load-path "~/.emacs.d/packages/evil/")
+(require 'evil)
+(evil-mode 1)
+
+;; Configure evil-mode keys
+
+;; Rebind M-x to ";"
+(define-key evil-normal-state-map ";" 'execute-extended-command)
+
+(evil-define-key 'normal markdown-mode-map "\C-m" 'markdown-enter-key)
+
