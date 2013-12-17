7538103769f75aaf35b0c7a83bac5c7faeb122ab
Alexey Bezhan <al@fueledbylemons.com>
2013-12-17 20:02:36 +0000

    Emacs: enable paredit in clojure-mode

--- emacs/emacs.d/packages.el
+++ emacs/emacs.d/packages.el
@@ -57,6 +57,7 @@
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
+(add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
 
 ;; Setup deft
 (require 'deft)
