6237fc53c7fd27bd7edd355dd3757cafd5ea206c
Alexey Bezhan <al@fueledbylemons.com>
2014-10-06 00:19:03 +0100

    Zsh: inline npm and pip completion plugins

--- zsh/zsh/plugins/npm.zsh
+++ zsh/zsh/plugins/npm.zsh
@@ -1,1 +1,11 @@
-eval "$(npm completion 2>/dev/null)"
+# from `npm completion`
+
+_npm_completion() {
+    compadd -- $(COMP_CWORD=$((CURRENT-1)) \
+                COMP_LINE=$BUFFER \
+                COMP_POINT=0 \
+                npm completion -- "${words[@]}" \
+                2>/dev/null)
+}
+
+compdef _npm_completion npm
