e12fbc6d6a182afdb87bbc44fdf58fdf14f8a907
Alexey Bezhan <al@fueledbylemons.com>
2018-09-30 22:21:05 +0100

    Replace vendored virtualenv load script with built-in one
    Virtualenvwrapper now ships a lazy loading script that creates the
    commands but only loads them on first use, speeding up zsh start-up.

--- zsh/zsh/plugins/virtualenv.zsh
+++ zsh/zsh/plugins/virtualenv.zsh
@@ -1,56 +1,9 @@
-# Dynamically load virtualenvwrapper functions to reduce shell startup
-# time.
-#
-# Copyright 2012 Aron Griffis <aron@arongriffis.com>
-# Released under the GNU GPL v3
-#######################################################################
-
 # Set up virtualenv variables
 export WORKON_HOME=$HOME/.virtualenvs
 
 # Set default python to Python 3
 export VIRTUALENV_PYTHON=python3
 
-# Python virtualenvwrapper loads really slowly, so load it on demand.
-if [[ $(type -w workon) != "workon: function" ]]; then
-virtualenv_funcs=( workon deactivate mkvirtualenv )
+export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
 
-  load_virtualenv() {
-    # If these already exist, then virtualenvwrapper won't override them.
-    unset -f "${virtualenv_funcs[@]}"
-
-    # virtualenvwrapper doesn't load if PYTHONPATH is set, because the
-    # virtualenv python doesn't have the right modules.
-    declare _pp="$PYTHONPATH"
-    unset PYTHONPATH
-
-    # Attempt to load virtualenvwrapper from its many possible sources...
-    _try_source() { [[ -f $1 ]] || return; source "$1"; return 0; }
-    _try_source /usr/local/share/python/virtualenvwrapper.sh || \
-    _try_source /usr/local/bin/virtualenvwrapper.sh || \
-    _try_source /usr/bin/virtualenvwrapper.sh
-    return_status=$?
-    unset -f _try_source
-
-    # Restore PYTHONPATH
-    [[ -n $_pp ]] && export PYTHONPATH="$_pp"
-
-    # Did loading work?
-    if [[ $return_status != 0 || $(type -w "$1") != "$1: function" ]]; then
-echo "Error loading virtualenvwrapper, sorry" >&2
-      return $return_status
-    fi
-
-    # Chain-load the appropriate function
-    "$@"
-  }
-
-  for v in "${virtualenv_funcs[@]}"; do
-eval "$v() { load_virtualenv $v \"\$@\"; }"
-  done
-
-  unset v
-fi
-
-# Enable simple workon completion before virtualenv load
-compctl -g $WORKON_HOME'/*(/:t)' workon
+source /usr/local/bin/virtualenvwrapper_lazy.sh
