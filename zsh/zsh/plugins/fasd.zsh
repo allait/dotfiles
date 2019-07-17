66b8ab1dc590466af4ff02713aa42deee5b58699
Alexey Bezhan <al@fueledbylemons.com>
2019-07-17 11:07:14 +0100

    Add fasd-based vim shortcut
    Opens vim with a fuzzy-match of the recent files from .viminfo

--- zsh/zsh/plugins/fasd.zsh
+++ zsh/zsh/plugins/fasd.zsh
@@ -5,16 +5,35 @@ if source $(which fasd) 2>/dev/null; then
 fi
 
 fasd_cd() {
-  if [ $# -le 1 ]; then
-    fasd "$@"
+  if [ $# -le 0 ]; then
+    cd "$(fasd -dl | fzf)"
   else
-    local _fasd_ret="$(fasd -e 'printf %s' "$@")"
-    [ -z "$_fasd_ret" ] && return
-    [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || printf %s\\n "$_fasd_ret"
+    cd "$(fasd -d -e 'printf %s' "$@")"
   fi
 }
 
-# Alias v and j
-alias j="fasd_cd -d"
-alias v="fasd -f -e vim"
+_fasd_edit_zsh_cmd_complete() {
+  local compl
+  read -c compl
+  (( $+compstate )) && compstate[insert]=menu # no expand if compsys loaded
+  reply=(${(f)"$(LANG=C fasd -b viminfo --complete "$compl")"})
+}
 
+fasd_edit() {
+  local dest
+  if [ $# -le 0 ]; then
+    dest="$(LANG=C fasd -tl -b viminfo | fzf)"
+  else
+    dest="$(LANG=C fasd -t -b viminfo "$@")"
+  fi
+
+  if [[ ! -z $dest ]]; then
+      vim "$dest"
+  fi
+}
+
+compctl -U -K _fasd_edit_zsh_cmd_complete -V fasd_edit -x 'C[-1,-*e],s[-]n[1,e]' -c - 'c[-1,-A][-1,-D]' -f -- fasd_edit
+
+# Alias v and j
+alias j="fasd_cd"
+alias v="fasd_edit"
