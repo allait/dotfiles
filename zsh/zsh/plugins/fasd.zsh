e0a99de5a8a6ba789b4b46d79a55c3f51f43b3ab
Alexey Bezhan <al@fueledbylemons.com>
2021-07-27 23:15:13 +0100

    Move `v` quickopen from fasd to fzf

--- zsh/zsh/plugins/fasd.zsh
+++ zsh/zsh/plugins/fasd.zsh
@@ -8,32 +8,9 @@ fasd_cd() {
   if [ $# -le 0 ]; then
     cd "$(fasd -dl | fzf)"
   else
-    cd "$(fasd -d -e 'printf %s' "$@")"
+    cd "$(fasd -dl1 "$@")"
   fi
 }
 
-_fasd_edit_zsh_cmd_complete() {
-  local compl
-  read -c compl
-  (( $+compstate )) && compstate[insert]=menu # no expand if compsys loaded
-  reply=(${(f)"$(LANG=C fasd -b viminfo --complete "$compl")"})
-}
-
-fasd_edit() {
-  local dest
-  if [ $# -le 0 ]; then
-    dest="$(LANG=C fasd -tl -b viminfo | fzf)"
-  else
-    dest="$(LANG=C fasd -t -b viminfo "$@")"
-  fi
-
-  if [[ ! -z $dest ]]; then
-      vim "$dest"
-  fi
-}
-
-compctl -U -K _fasd_edit_zsh_cmd_complete -V fasd_edit -x 'C[-1,-*e],s[-]n[1,e]' -c - 'c[-1,-A][-1,-D]' -f -- fasd_edit
-
-# Alias v and j
+# Alias j
 alias j="fasd_cd"
-alias v="fasd_edit"
--- zsh/zsh/plugins/fzf.zsh
+++ zsh/zsh/plugins/fzf.zsh
@@ -3,3 +3,34 @@
 
 source /usr/local/opt/fzf/shell/completion.zsh 2>/dev/null
 source /usr/local/opt/fzf/shell/key-bindings.zsh 2>/dev/null
+
+# Use fd for CTRL-T completion
+export FZF_CTRL_T_COMMAND="fd --follow"
+
+# Use fd for `fzf` command
+export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
+
+# Use fd for ** path completion
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
+# _v_complete() {
+#     _fzf_complete "--multi --reverse --filter" "$@" <(
+#         fd -d5
+#     )
+# }
+
+v() {
+  local editor="nvim"
+  local files
+  IFS=$'\n' files=($(fzf --query="$@" --cycle --select-1 --exit-0))
+  [[ -n "$files" ]] && ${editor} "${files[@]}"
+}
+
+# compctl -U -K _v_complete -x 'C[-1,-*e],s[-]n[1,e]' -c - 'c[-1,-A][-1,-D]' -f -- v
