4d8ca7b0b5f2994aee2767e6ebc030c0526ad110
Alexey Bezhan <al@fueledbylemons.com>
2021-07-27 23:16:08 +0100

    Set python3 path for virtualenvwrapper

--- zsh/zsh/plugins/virtualenv.zsh
+++ zsh/zsh/plugins/virtualenv.zsh
@@ -3,6 +3,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 
 # Set default python to Python 3
 export VIRTUALENV_PYTHON=python3
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 
 export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
 
